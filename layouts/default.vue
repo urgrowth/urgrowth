a8321b4b368b9fe1b61133abaa7ad5978783266e
tinarskii <tinvv@outlook.co.th>
2023-10-05 00:01:52 +0700

    feat: categorize pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,43 @@
       </div>
       <div class="drawer-side">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu p-4 w-64 min-h-full backdrop-blur-xl bg-white/15 border-r border-white/25 text-white font-bold">
+        <ul class="menu p-4 w-64 min-h-full backdrop-blur-xl bg-gradient-to-b from-blue-400/60 to-red-500/20 border-r border-white/25 text-white text-drop-shadow font-bold">
           <li><NuxtLink to="/"><img src="/icons/home.png" alt="Home" class="w-6">Home</NuxtLink></li>
-          <li><NuxtLink to="/advices"><img src="/icons/info.png" alt="Advices" class="w-6">Advices</NuxtLink></li>
-          <li><NuxtLink to="/learning"><img src="/icons/book.png" alt="Learning" class="w-6">Learning</NuxtLink></li>
-          <li><NuxtLink to="/my-plans"><img src="/icons/target.png" alt="My Plans" class="w-6">My Plans</NuxtLink></li>
-          <li><NuxtLink to="/suggestions"><img src="/icons/discuss.png" alt="Suggestions" class="w-6">Suggestions</NuxtLink></li>
+          <li><NuxtLink to="/notifications"><img src="/icons/bell.png" alt="Notifications" class="w-6">Notifications</NuxtLink></li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/suggestions"><img src="/icons/discuss.png" alt="Suggestions" class="w-6">Suggestions</NuxtLink>
+            <NuxtLink to="/suggestions/model" class="ml-8">Model</NuxtLink>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/learning"><img src="/icons/book.png" alt="Learning" class="w-6">Learning</NuxtLink>
+            <NuxtLink to="/learning/student" class="ml-8">Student</NuxtLink>
+            <NuxtLink to="/learning/teacher" class="ml-8">Teacher</NuxtLink>
+            <NuxtLink to="/learning/quest-user" class="ml-8">Quest User</NuxtLink>
+            <NuxtLink to="/learning/header-group" class="ml-8">Header Group</NuxtLink>
+            <NuxtLink to="/learning/group-member" class="ml-8">Group Member</NuxtLink>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/my-plans"><img src="/icons/target.png" alt="My Plans" class="w-6">My Plans</NuxtLink>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/consult"><img src="/icons/discuss.png" alt="Consult" class="w-6">Consult</NuxtLink>
+            <NuxtLink to="/consult/marketing" class="ml-8">Marketing</NuxtLink>
+            <NuxtLink to="/consult/technical" class="ml-8">Technical</NuxtLink>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/community"><img src="/icons/community.png" alt="Community" class="w-6">Community</NuxtLink>
+            <NuxtLink to="/community/direct-messages" class="ml-8">Direct Messages</NuxtLink>
+            <NuxtLink to="/community/group-messages" class="ml-8">Group Messages</NuxtLink>
+          </li>
+          <div class="divider"></div>
+          <li>
+            <NuxtLink to="/accomplishment"><img src="/icons/grow.png" alt="Accomplishment" class="w-6">Accomplishment</NuxtLink>
+          </li>
         </ul>
       </div>
     </div>
@@ -26,12 +57,17 @@
 .drawer-side {
   z-index: 2;
 }
-.menu img {
-  @apply invert;
+
+a img {
+  @apply invert
 }
 
 a {
-  @apply hover:text-white hover:bg-black/60 p-2
+  @apply hover:bg-black/80 p-2 hover:text-white;
+}
+
+.text-drop-shadow {
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 </style>
 <script setup lang="ts">
