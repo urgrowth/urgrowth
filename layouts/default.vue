7f2c918e4366d7decb6ffbc4c91e1e08f7a563e8
Anner <98201562+Annerez@users.noreply.github.com>
2023-10-17 15:01:37 +0700

    fix side navbar to be more elegant (#11)

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,108 +8,81 @@
         <slot />
       </div>
       <div class="drawer-side">
-        <label
-          for="my-drawer"
-          aria-label="close sidebar"
-          class="drawer-overlay"
-        ></label>
+        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
         <ul
-          class="text-drop-shadow menu min-h-full w-64 border-r border-white/25 bg-gradient-to-b from-blue-400/60 to-red-500/20 p-4 font-bold text-white backdrop-blur-xl"
+          class="menu gap-y-4 min-h-full w-64 border-r border-white/25 bg-gradient-to-b from-blue-400/60 to-red-500/20 p-4 font-bold text-white backdrop-blur-xl"
         >
-          <li>
-            <NuxtLink to="/"
-              ><img
-                src="/icons/home.png"
-                alt="Home"
-                class="w-6"
-              />Home</NuxtLink
-            >
+          <li class="flex flex-row">
+              <img src="/images/logo-white.svg" alt="Logo" class="w-8 ml-1 p-0" />
+              <h1 class="pl-2 pt-3">URGROWTH</h1>
+              <label for="my-drawer">
+                <img class="icon invert hover:invert-0" src="/icons/hamburger.png" alt="Menu" />
+              </label>
           </li>
           <li>
-            <NuxtLink to="/notifications"
-              ><img
-                src="/icons/bell.png"
-                alt="Notifications"
-                class="w-6"
-              />Notifications</NuxtLink
-            >
+            <NuxtLink to="/">
+              <img src="/icons/home.png" alt="Home" class="w-4"/>
+              Home
+            </NuxtLink>
           </li>
-          <div class="divider"></div>
           <li>
-            <NuxtLink to="/suggestions"
-              ><img
-                src="/icons/info.png"
-                alt="Suggestions"
-                class="w-6"
-              />Suggestions</NuxtLink
-            >
-            <NuxtLink to="/suggestions/model" class="ml-8">Model</NuxtLink>
+            <NuxtLink to="/notifications">
+              <img src="/icons/bell.png" alt="Notifications" class="w-4"/>
+              Notifications
+              </NuxtLink>
           </li>
-          <div class="divider"></div>
           <li>
-            <NuxtLink to="/learning"
-              ><img
-                src="/icons/book.png"
-                alt="Learning"
-                class="w-6"
-              />Learning</NuxtLink
-            >
-            <NuxtLink to="/learning/student" class="ml-8">Student</NuxtLink>
-            <NuxtLink to="/learning/teacher" class="ml-8">Teacher</NuxtLink>
-            <NuxtLink to="/learning/quest-user" class="ml-8"
-              >Quest User</NuxtLink
-            >
-            <NuxtLink to="/learning/header-group" class="ml-8"
-              >Header Group</NuxtLink
-            >
-            <NuxtLink to="/learning/group-member" class="ml-8"
-              >Group Member</NuxtLink
-            >
+            <NuxtLink @click="toggleAccordion('suggestions')">
+              <img src="/icons/info.png" alt="Suggestions" class="w-4" />
+              Suggestions
+              <img src="/images/caret.png" alt="caret" class="w-2 caret" :class="{ 'open': activeAccordion === 'suggestions'}" />
+            </NuxtLink>
+              <NuxtLink v-if="activeAccordion === 'suggestions'" to="/suggestions/model" class="ml-8 mt-2">Model</NuxtLink>
           </li>
-          <div class="divider"></div>
+          <li>
+            <NuxtLink @click="toggleAccordion('learnings')">
+              <img src="/icons/book.png" alt="Learning" class="w-4" />
+              Learnings
+              <img src="/images/caret.png" alt="caret" class="w-2 caret" :class="{ 'open': activeAccordion === 'learnings'}" />
+            </NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'learnings'" to="/learning/student" class="ml-8 mt-2">Student</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'learnings'" to="/learning/teacher" class="ml-8">Teacher</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'learnings'" to="/learning/quest-user" class="ml-8">Quest User</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'learnings'" to="/learning/header-group" class="ml-8">Header Group</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'learnings'" to="/learning/group-member" class="ml-8">Group Member</NuxtLink>
+          </li>
+
           <li>
             <NuxtLink to="/my-plans"
-              ><img src="/icons/target.png" alt="My Plans" class="w-6" />My
+              ><img src="/icons/target.png" alt="My Plans" class="w-4" />My
               Plans</NuxtLink
             >
           </li>
-          <div class="divider"></div>
+
           <li>
-            <NuxtLink to="/consult"
-              ><img
-                src="/icons/discuss.png"
-                alt="Consult"
-                class="w-6"
-              />Consult</NuxtLink
-            >
-            <NuxtLink to="/consult/marketing" class="ml-8">Marketing</NuxtLink>
-            <NuxtLink to="/consult/technical" class="ml-8">Technical</NuxtLink>
+            <NuxtLink @click="toggleAccordion('consult')">
+              <img src="/icons/discuss.png" alt="Consult" class="w-4" />
+              Consult
+              <img src="/images/caret.png" alt="Suggestions" class="w-2 caret" :class="{ 'open': activeAccordion === 'consult'}" />
+            </NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'consult'" to="/consult/marketing" class="ml-8 mt-2">Marketing</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'consult'" to="/consult/technical" class="ml-8">Technical</NuxtLink>
           </li>
-          <div class="divider"></div>
+
           <li>
-            <NuxtLink to="/community"
-              ><img
-                src="/icons/community.png"
-                alt="Community"
-                class="w-6"
-              />Community</NuxtLink
-            >
-            <NuxtLink to="/community/direct-messages" class="ml-8"
-              >Direct Messages</NuxtLink
-            >
-            <NuxtLink to="/community/group-messages" class="ml-8"
-              >Group Messages</NuxtLink
-            >
+            <NuxtLink @click="toggleAccordion('community')">
+              <img src="/icons/community.png" alt="Community" class="w-4" />
+              Community
+              <img src="/images/caret.png" alt="Suggestions" class="w-2 caret" :class="{ 'open': activeAccordion === 'community'}" />
+            </NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'community'" to="/community/direct-messages" class="ml-8">Direct Messages</NuxtLink>
+            <NuxtLink v-if="activeAccordion === 'community'" to="/community/group-messages" class="ml-8">Group Messages</NuxtLink>
           </li>
-          <div class="divider"></div>
+
           <li>
-            <NuxtLink to="/accomplishment"
-              ><img
-                src="/icons/grow.png"
-                alt="Accomplishment"
-                class="w-6"
-              />Accomplishment</NuxtLink
-            >
+            <NuxtLink to="/accomplishment">
+              <img src="/icons/grow.png" alt="Accomplishment" class="w-4"/>Accomplishment
+            </NuxtLink>
           </li>
         </ul>
       </div>
@@ -124,14 +97,53 @@
 }
 
 a img {
-  @apply invert;
+  @apply invert hover:invert-0;
 }
 
 a {
-  @apply p-2 hover:bg-black/80 hover:text-white;
+  @apply p-2 hover:bg-black/80 rounded-lg hover:text-white;
+  position: relative;
 }
 
+.caret {
+  position: absolute;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%) rotate(180deg);
+  transition: transform 0.2s;
+}
+
+.open {
+  transform: translateY(-50%) rotate(0deg);
+}
 .text-drop-shadow {
   text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      activeAccordion: null, // Initialize activeAccordion as null
+    };
+  },
+  methods: {
+    toggleAccordion(section) { //#11101D
+      if (this.activeAccordion === section) {
+        this.activeAccordion = null; // Close the accordion if it's already open
+      } else {
+        this.activeAccordion = section; // Open the accordion
+      }
+    },
+  },
+};
+//        <ulclass="menu gap-y-4 min-h-full w-64 border-r border-white/25 bg-black p-4 font-bold text-white">
+//darkmode
+//<div class="divider"></div>
+
+</script>
+
+
+
+
